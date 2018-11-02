ac175c35f724f8ab23d1b921727e719ffd1d7cad
Ahmed Hammad <hammad97official@gmail.com>
2018-11-03 00:48:38 +0200

    Merge : Single threaded version done

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include <thread>
 
 using namespace std;
 
-void readInput(vector<int>>& arr){
+void readInput(vector<int>& arr){
     ifstream inFile;
     inFile.open("input.txt");
     int n;
@@ -18,8 +20,52 @@ void readInput(vector<int>>& arr){
     inFile.close();
 }
 
+void merge(vector<int>& arr, int start, int middle, int end){
+    vector<int> temp;
+    int i=start, j=middle+1;
+
+    while(i <= middle && j <= end){
+        if(arr[i]<arr[j]){
+            temp.push_back(arr[i++]);
+        }
+        else{
+            temp.push_back(arr[j++]);
+        }
+    }
+
+    while(i <= middle){
+        temp.push_back(arr[i++]);
+    }
+
+    while(j <= end){
+        temp.push_back(arr[j++]);
+    }
+
+    for(int i=0; i<temp.size(); i++){
+        arr[start+i] = temp[i];
+    }
+}
+
+void mergeSort(vector<int>& arr, int start, int end){
+    if(end>start){
+        mergeSort(arr, start, (start+end)/2);
+        mergeSort(arr, ((start+end)/2)+1, end);
+        merge(arr, start, (start+end)/2, end);
+    }
+}
+
+
+void printArray(vector<int>& arr){
+    for(auto& num : arr){
+        cout<<num<<" ";
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> arr;
+    readInput(arr);
+    mergeSort(arr, 0, arr.size()-1);
+    printArray(arr);
     return 0;
 }
